ba42c48c64bb0144ea2adbdea07d9e2f94683371
ameyakakade <ameyakakade@gmail.com>
2026-03-12 17:21:09 +0530

    Added shaders. Data is transfered to gpu as a texture. Used gemini to help with boilerplate and getting OpenGL working inside JUCE

--- vst/PluginEditor.cpp
+++ vst/PluginEditor.cpp
@@ -1,16 +1,18 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-int height = 400;
-int width  = 400;
+int height = 600;
+int width  = 800;
 
 //==============================================================================
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
+    : AudioProcessorEditor (&p), processorRef (p), sh(p)
 {
     juce::ignoreUnused (processorRef);
-    setSize (height, width);
+    setSize (width, height);
     startTimerHz(60);
+    size = processorRef.fft_analyze(0.016f);
+    addAndMakeVisible(sh);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -20,18 +22,17 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 //==============================================================================
 void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    float m = processorRef.fft_analyze(0.016f);
-    int step = width/m;
-    g.setColour (juce::Colours::white);
-    for(size_t i=0; i<m-1; i++)
-    {
-        float t = processorRef.out_smooth[i];
-        g.fillRect((float)(i*step) , 0.0, (float)step/2, t*2/3*height);
-    }
+    // g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    //
+    // float m = processorRef.fft_analyze(0.016f);
+    // int step = width/m;
+    // g.setColour (juce::Colours::white);
+    // for(size_t i=0; i<m-1; i++)
+    // {
+    //     float t = processorRef.out_smooth[i];
+    //     g.fillRect((float)(i*step) , 0.0, (float)step/2, t*2/3*height);
+    // }
 }
-
 void AudioPluginAudioProcessorEditor::timerCallback()
 {
     repaint();
@@ -39,4 +40,110 @@ void AudioPluginAudioProcessorEditor::timerCallback()
 
 void AudioPluginAudioProcessorEditor::resized()
 {
+    sh.setBounds(getLocalBounds());
+}
+
+shaderC::shaderC(AudioPluginAudioProcessor& p) : processorRef(p)
+{
+    setOpaque(true);
+    openGLContext.setComponentPaintingEnabled(false);
+    openGLContext.setContinuousRepainting(true);
+    textureWidth = processorRef.fft_analyze(0.016f); 
+}
+
+shaderC::~shaderC()
+{
+
+}
+
+void shaderC::resized()
+{
+
+}
+
+void shaderC::initialise()
+{
+
+    shader.reset(new juce::OpenGLShaderProgram(openGLContext));
+
+    if(shader -> addVertexShader(vertexSource) && shader -> addFragmentShader(fragmentSource) && shader -> link())
+    {
+        DBG(shader->getLastError());
+    }
+
+    float vertices[] = {
+        -1.0f,  1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+        -1.0f,  1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+         1.0f,  1.0f, 0.0f,
+    };
+
+
+    openGLContext.extensions.glGenVertexArrays(1, &VAO);
+
+    openGLContext.extensions.glGenBuffers(1, &VBO);
+
+    openGLContext.extensions.glBindVertexArray(VAO);
+
+    openGLContext.extensions.glBindBuffer(juce::gl::GL_ARRAY_BUFFER, VBO);
+
+    openGLContext.extensions.glBufferData(juce::gl::GL_ARRAY_BUFFER, sizeof(vertices), vertices, juce::gl::GL_STATIC_DRAW);
+
+    openGLContext.extensions.glVertexAttribPointer(0, 3, juce::gl::GL_FLOAT, juce::gl::GL_FALSE, 3 * sizeof(float), 0);
+    openGLContext.extensions.glEnableVertexAttribArray(0);
+
+    juce::gl::glGenTextures(1, &dataTextureID);
+    juce::gl::glBindTexture(juce::gl::GL_TEXTURE_2D, dataTextureID);
+
+    // Setup scaling filters (Linear will interpolate between samples for a smooth look)
+    juce::gl::glTexParameteri(juce::gl::GL_TEXTURE_2D, juce::gl::GL_TEXTURE_MIN_FILTER, juce::gl::GL_NEAREST);
+    juce::gl::glTexParameteri(juce::gl::GL_TEXTURE_2D, juce::gl::GL_TEXTURE_MAG_FILTER, juce::gl::GL_NEAREST);
+    juce::gl::glTexParameteri(juce::gl::GL_TEXTURE_2D, juce::gl::GL_TEXTURE_WRAP_S, juce::gl::GL_CLAMP_TO_EDGE);
+    juce::gl::glTexParameteri(juce::gl::GL_TEXTURE_2D, juce::gl::GL_TEXTURE_WRAP_T, juce::gl::GL_CLAMP_TO_EDGE);
+
+    // Initialize with NULL (empty container)
+    // GL_R32F is the "Native Float" format for modern GPUs
+    juce::gl::glTexImage2D(juce::gl::GL_TEXTURE_2D, 0, juce::gl::GL_R32F, textureWidth, 1, 0, juce::gl::GL_RED, juce::gl::GL_FLOAT, nullptr);
+
+}
+
+void shaderC::render()
+{
+
+    // Clear background
+    juce::OpenGLHelpers::clear(juce::Colour(0xddddddff));
+
+    float m = processorRef.fft_analyze(0.016f);
+    
+    // 2. Bind and Upload
+    openGLContext.extensions.glActiveTexture(juce::gl::GL_TEXTURE0);
+    juce::gl::glBindTexture(juce::gl::GL_TEXTURE_2D, dataTextureID);
+
+    // glTexSubImage2D is much faster than glTexImage2D because it doesn't re-allocate
+    juce::gl::glTexSubImage2D(juce::gl::GL_TEXTURE_2D, 0, 0, 0, textureWidth, 1, juce::gl::GL_LUMINANCE, juce::gl::GL_FLOAT, processorRef.out_smooth);
+
+    // 3. Link to the shader
+    shader->setUniform("uDataTexture", 0); // Use Texture Unit 0
+
+    shader->setUniform("uRes", (float)getWidth(), (float)getHeight());
+
+    // Pass uniforms (like time)
+    shader->setUniform("steps", m);
+
+    shader->use();
+
+    // Draw the quad
+    openGLContext.extensions.glBindVertexArray(VAO);
+    juce::gl::glDrawArrays(juce::gl::GL_TRIANGLES, 0, 6);
+
+    
+}
+
+void shaderC::shutdown()
+{
+    shader.reset();
+    openGLContext.extensions.glDeleteVertexArrays(1, &VAO);
+    openGLContext.extensions.glDeleteBuffers(1, &VBO);
 }
--- vst/PluginEditor.h
+++ vst/PluginEditor.h
@@ -1,9 +1,64 @@
 #pragma once
 
 #include "PluginProcessor.h"
+#include <juce_opengl/juce_opengl.h>
+
+class shaderC : public juce::OpenGLAppComponent
+{
+public:
+    shaderC(AudioPluginAudioProcessor& p);
+    ~shaderC() override;
+
+    //==============================================================================
+    // void paint (juce::Graphics&) override;
+    void resized() override;
+    void initialise() override; 
+    void shutdown() override; 
+    void render() override;
+
+private:
+
+    AudioPluginAudioProcessor& processorRef;
+
+    std::unique_ptr<juce::OpenGLShaderProgram> shader;
+    GLuint VBO, VAO;
+    GLuint dataTextureID = 0;
+    int textureWidth;
+    float startTime = juce::Time::getMillisecondCounterHiRes();
+
+    juce::String vertexSource = 
+        R"glsl(#version 120
+    attribute vec3 aPos;
+    void main() {
+        gl_Position = vec4(aPos, 1.0);
+    }
+    )glsl" ;
+
+    juce::String fragmentSource =
+         R"glsl(#version 120
+        uniform sampler2D uDataTexture;
+        uniform vec2 uRes;
+        uniform float steps;
+
+        void main() {
+
+            vec2 uv = gl_FragCoord.xy / uRes;
+
+            float height = texture2D(uDataTexture, vec2(uv.x*0.5, 0.5)).r*2/3;
+
+            float no = uv.x*steps*0.5 - floor(uv.x*steps*0.5);
+
+            float isVisible = floor(height/uv.y);
+
+            gl_FragColor = vec4(no, isVisible, 0.0, 1.0);
+        }
+    )glsl" ;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (shaderC)
+};
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor, juce::Timer
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor, public juce::Timer
 {
 public:
     explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
@@ -15,7 +70,13 @@ public:
     void timerCallback() override;
 
 private:
+
     AudioPluginAudioProcessor& processorRef;
+
+    shaderC sh;
+
+    size_t size = 512;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
+
